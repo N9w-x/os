da149228dd561a4b4b17e1d78f89492315b9516c
wnx233 <1602946346@qq.com>
2022-08-15 22:45:00 +0800

    合并分支

--- kernel/src/fs/dev_fs.rs
+++ kernel/src/fs/dev_fs.rs
@@ -66,42 +66,6 @@ impl File for Rtc {
     }
 }
 
-impl File for Rtc {
-    fn readable(&self) -> bool {
-        true
-    }
-    
-    fn writable(&self) -> bool {
-        true
-    }
-    
-    fn read(&self, mut buf: UserBuffer) -> usize {
-        buf.clear()
-    }
-    
-    fn write(&self, buf: UserBuffer) -> usize {
-        buf.len()
-    }
-}
-
-impl File for Rtc {
-    fn readable(&self) -> bool {
-        true
-    }
-    
-    fn writable(&self) -> bool {
-        true
-    }
-    
-    fn read(&self, mut buf: UserBuffer) -> usize {
-        buf.clear()
-    }
-    
-    fn write(&self, buf: UserBuffer) -> usize {
-        buf.len()
-    }
-}
-
 pub fn open_dev_file(path: &str) -> Option<Arc<dyn File + Send + Sync>> {
     if path.ends_with("zero") {
         Some(Arc::new(Zero::default()))
